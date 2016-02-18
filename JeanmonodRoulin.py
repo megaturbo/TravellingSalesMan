c725ef8271fb80df3ec9b5ac62cd20a8f81bc64e
Thomas Roulin <thomasroulinCH@gmail.com>
2016-02-18 21:12:55 +0100

    Dont stash

--- JeanmonodRoulin.py
+++ JeanmonodRoulin.py
@@ -100,6 +100,7 @@ def crossover(father, mother):
 
 def evolve(chromosomes, dists):
     popsize = len(chromosomes)
+    print(popsize)
     # selection
     newpop = wheelselect(chromosomes, popsize)
     # crossover
